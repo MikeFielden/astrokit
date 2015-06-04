f330acc96faf36849995fc7bf647a36c2e09ba2e
Collin Estes <Collin.j.estes@gmail.com>
2015-06-04 16:59:23 -0500

    Transpilers beginning

---
+++ transpilers/babel/gulpfile.js
@@ -0,0 +1,34 @@
+/**
+ * Libraries
+ *
+ **/
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+var globals = require('./setup/globals.json');
+
+gulp.task('default', function () {
+    /**
+     *  Folder structure looks like this on the container:
+     *
+     *  /src
+     *      /app
+     *          /test
+     *              // LOOKS IN HERE FOR TESTS
+     *      /astro_code
+     *          THIS PROJECT
+     *              Dockerfile
+     *              gulpfile.js
+     *              package.json
+     *
+     **/
+    require('babel/register')({ modules: 'common' });
+    return gulp.src(['./setup/node.js', '../app/test/**/*.js'], {read: false})
+    .pipe(plugins.mocha({reporter: 'spec', globals: globals}))
+    .on('error', plugins.util.log);
+});
+
+
+gulp.task('babel-', function() {
+    
+});
