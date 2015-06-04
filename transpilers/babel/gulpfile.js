06c6e55420f95d6023d56283878def38ac2a4fa7
Collin Estes <Collin.j.estes@gmail.com>
2015-06-04 17:30:56 -0500

    transpilers closer
    Still not quite right, but close

--- transpilers/babel/gulpfile.js
+++ transpilers/babel/gulpfile.js
@@ -5,30 +5,54 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
-var globals = require('./setup/globals.json');
+var run = require('run-sequence');
 
+var arg = require('yargs').argv;
+
+/**
+ *  Folder structure looks like this on the container:
+ *
+ *  /src
+ *      /app
+ *          /app
+ *              // LOOKS IN HERE FOR SERVER CODE
+ *      /public
+ *          /js
+ *              // LOOKS IN HERE FOR CLIENT CODE
+ *      /astro_code
+ *          THIS PROJECT
+ *              Dockerfile
+ *              gulpfile.js
+ *              package.json
+ *
+ **/
 gulp.task('default', function () {
-    /**
-     *  Folder structure looks like this on the container:
-     *
-     *  /src
-     *      /app
-     *          /test
-     *              // LOOKS IN HERE FOR TESTS
-     *      /astro_code
-     *          THIS PROJECT
-     *              Dockerfile
-     *              gulpfile.js
-     *              package.json
-     *
-     **/
-    require('babel/register')({ modules: 'common' });
-    return gulp.src(['./setup/node.js', '../app/test/**/*.js'], {read: false})
-    .pipe(plugins.mocha({reporter: 'spec', globals: globals}))
-    .on('error', plugins.util.log);
+
+  // check for server only flag
+  if (arg.server)
+    return run('babel-server');
+
+  // check for client only flag
+  if (arg.client)
+    return run('babel-client');
+
+
+  // run both if no flags
+  return run(['babel-server', 'babel-client']);
+
 });
 
 
-gulp.task('babel-', function() {
-    
+
+gulp.task('babel-server', function() {
+  return gulp.src(['../app/app/**/*.js'])
+  .pipe(plugins.babel())
+  .pipe(gulp.dest('../app/dist'));
 });
+
+gulp.task('babel-client', function () {
+    return gulp.src(['../app/public/js/**/*.js'])
+    .pipe(plugins.babel())
+    .pipe(gulp.dest('../app/public/dist/js'));
+});
+
