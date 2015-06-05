b6e60aff579854f4244584e8d2e9e22301a13bd2
Mike Fielden <fielden.mike@gmail.com>
2015-06-05 11:22:17 -0500

    scss -> css conversion task is complete

--- transpilers/sass/gulpfile.js
+++ transpilers/sass/gulpfile.js
@@ -9,8 +9,8 @@ var plugins = require('gulp-load-plugins')();
  * Settings
  **/
 var paths = {
-  client: 'public/',
-  build: 'public/dist/'
+  client: '../app/public/',
+  build: '../app/public/dist/css'
 };
 
 var assets = {
@@ -49,14 +49,16 @@ gulp.task('default', function () {
      *              package.json
      **/
     return gulp.src(paths.client + assets.css + files.css)
+      .pipe(plugins.filelog())
       .pipe(plugins.plumber())
       .pipe(plugins.sass())
       .on('error', plugins.util.log)
       .pipe(plugins.autoprefixer())
       .pipe(plugins.size({"showFiles":true}))
-      .pipe(gulp.dest(paths.build + assets.css))
+      .pipe(gulp.dest(paths.build + '/'))
       .pipe(plugins.minifyCss())
       .pipe(plugins.rename({suffix: '.min'}))
       .pipe(plugins.size({"showFiles":true}))
-      .pipe(gulp.dest(paths.build + assets.css));
+      .pipe(gulp.dest(paths.build + '/'))
+      .pipe(plugins.duration('Task duration'));
 });
