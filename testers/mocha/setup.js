6a4ed507245756958a9af43884871e2dc9a30a9e
Collin Estes <Collin.j.estes@gmail.com>
2015-06-09 12:09:18 -0500

    Added globals to testing process
    Can now use:
    
    expect, should, assert, sinon, sinon-chai

--- testers/mocha/setup.js
+++ testers/mocha/setup.js
@@ -1,3 +1,9 @@
-global.expect = require('chai').expect
+global.chai = require('chai');
+global.expect = chai.expect;
+global.should = chai.should;
+global.assert = chai.assert;
+global.sinon = require('sinon');
+global.chai.use(require('sinon-chai'));
+
 
 
