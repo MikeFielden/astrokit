13ce39ea8cd6bd4d1c34fb490b1a000475344037
Collin Estes <Collin.j.estes@gmail.com>
2015-06-09 11:41:20 -0500

    lint updates

--- linters/jshint/controller.sh
+++ linters/jshint/controller.sh
@@ -1,7 +1,10 @@
-
-#if [ $ASTROCMD = 'jshint:watch' ]
-#then
-	#inotifywait /src/app
-#else
-	./jshint.sh
-#fi
+#!/bin/sh
+if [ $ASTROCMD = 'jshint:watch' ]
+then
+	while inotifywait -r --exclude /src/app/node_modules -e modify /src/app/; do
+		echo 'hello world'
+		sh ./jshint.sh
+	done
+else
+	sh ./jshint.sh
+fi
