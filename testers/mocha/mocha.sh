11a797c0a1da5d4d07b7373a5bc920ed589dc026
Collin Estes <collin.j.estes@nasa.gov>
2015-06-12 11:36:42 -0500

    Added --recursive flag to mocha

--- testers/mocha/mocha.sh
+++ testers/mocha/mocha.sh
@@ -1,5 +1,5 @@
 echo '*************    starting mocha    *****************'
 
-mocha --compilers js:babel/register "./setup.js" "/src/app/test"
+mocha --compilers js:babel/register --recursive "./setup.js" "/src/app/test"
 
 echo '*************    completed mocha    *****************'
