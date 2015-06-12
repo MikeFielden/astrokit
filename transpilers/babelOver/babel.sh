4cfe1a8b888e64ca635118e53a186d9e12f36bab
Collin Estes <Collin.j.estes@gmail.com>
2015-06-12 00:05:59 -0500

    babelOver
    Made for testing projects that include local dependencies that require transpilation to run.  With 'astro babelOver-<module name>', astro will use babel to transpile the local dependency in the node_modules folder so testing can be performed with astro (which can't use npm link...)

---
+++ transpilers/babelOver/babel.sh
@@ -0,0 +1,5 @@
+echo '*************    starting babel:over    *****************'
+
+babel /src/app/ --out-dir /src/app/ --ignore /node_modules/*
+
+echo '*************    completed babel:over    *****************'
